797b0ed24bd043a8178219133f868f37dc89377b
sumant7 <91867296+sumant7@users.noreply.github.com>
2022-02-06 16:40:20 +0530

    test

--- arrays.cpp
+++ arrays.cpp
@@ -28,7 +28,7 @@ void rearrange(int arr[], int n)     //n is size of arr
 
 
 
-//largest sum subarray    or Kadane's Algorithm
+//largest sum subarray    or Kadane's Algorithm  (IMP)
 /*The simple idea of Kadane’s algorithm is to look for all positive contiguous segments of the array (max_ending_here is used for this). And keep track of maximum sum contiguous segment among all positive segments (max_so_far is used for this). Each time we get a positive-sum compare it with max_so_far and update max_so_far if it is greater than max_so_far*/
 int maxSubArraySum(int a[], int size)
 {
