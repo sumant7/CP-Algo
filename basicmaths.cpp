0a475b3142034dbb6c362f52585cd0670e66de22
sumant7 <91867296+sumant7@users.noreply.github.com>
2022-02-28 17:20:05 +0530

    binary exponentiation and remainder

--- basicmaths.cpp
+++ basicmaths.cpp
@@ -157,3 +157,16 @@ long long binpow(long long a, long long b) {
 
 
 
+
+// (a^b)%m using binary when b is very large
+long long binpowr(long long a, long long b, long long m) {
+    a %= m;
+    long long res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a % m;
+        a = a * a % m;
+        b >>= 1;
+    }
+    return res;
+}
