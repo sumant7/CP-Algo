8264fbb858bc844ed07bff454dfd47c39f57a689
sumant7 <91867296+sumant7@users.noreply.github.com>
2022-03-09 17:04:15 +0530

    stack Begins

---
+++ stack.cpp
@@ -0,0 +1,10 @@
+//they operate on Last in First Out
+//like chairs stacked on top of each other, to take out a chair you have to remove all chairs above it one by one
+
+#include <iostream>
+#include <stack>
+using namespace std;
+int main() {
+    stack <int> s;
+    
+}
