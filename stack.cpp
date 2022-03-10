86ce9f3a86531809d16434aa4cc58119e0647b62
sumant7 <91867296+sumant7@users.noreply.github.com>
2022-03-10 18:28:46 +0530

    next greater element

--- stack.cpp
+++ stack.cpp
@@ -2,8 +2,28 @@
 //like chairs stacked on top of each other, to take out a chair you have to remove all chairs above it one by one
 //size, push,empty,pop,top all occur in O(1)
 #include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <cstring>
+#include <cstdlib>
+#include <map>
+#include <list>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <queue>
 #include <stack>
 using namespace std;
+#define prn(n) cout<<n<<"\n"
+#define pr(n) cout<<n
+#define ps(n) cout<<n<<" "
+#define el <<'\n'
+#define sp <<" "
+#define ll long long int
+#define pb push_back
+#define cn continue
+#define fr(i,n) for(i=0;i<n;++i)
 int main() {
     stack <int> s;  //creating a stack of int
     //inserting elements into a stack one by one
@@ -37,3 +57,35 @@ both stacks must be of same data type but can be of different size. Time O(n)
 stack.emplace 
 While push() function inserts a copy of the value or the parameter passed to the function into the container at the top, the emplace() function constructs a new element as the value of the parameter and then adds it to the top of the container. 
 */
+
+
+
+//print next largest number, j>i && a[j]>a[i]
+vector<pair<ll,ll>> solve(vector<ll> a)  //returns [{element,nextlargest}]  !!! not in correct order
+{
+    vector<pair<ll,ll>> v;
+    stack <ll> s;
+    s.push(a[0]);
+    ll i;
+    for(i=1;i<a.size();++i)
+    {
+        ll x=a[i];
+        if(s.empty())
+        {
+            s.push(a[i]);
+            cn;
+        }
+        while(!s.empty() && s.top()<a[i])
+        {
+            v.pb({s.top(),a[i]});
+            s.pop();
+        }
+        s.push(a[i]);
+    }
+    while(!s.empty())
+    {
+        v.pb({s.top(),-1});
+        s.pop();
+    }
+    return v;
+}
