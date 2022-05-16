ec8c2389aaf50466427b22166603e31d9534ef6d
sumant7 <91867296+sumant7@users.noreply.github.com>
2022-05-16 13:44:31 +0530

    evaluating postfix using stack

--- stack.cpp
+++ stack.cpp
@@ -224,3 +224,43 @@ void infixToPostfix(string s)
     
     cout << ns << endl;
 }
+
+
+//evaluating postfix
+int evaluatePostfix(char* exp) //or string
+{ 
+    // Create a stack 
+    stack<int> st;
+    
+    int i; 
+
+    // Scan all characters one by one 
+    for (i = 0; exp[i]; ++i) 
+    { 
+        // If the scanned character is an operand (number here), 
+        // push it to the stack. 
+        if (isdigit(exp[i])) 
+            st.push(exp[i] - '0'); 
+
+        // If the scanned character is an operator, pop two 
+        // elements from stack apply the operator 
+        else
+        { 
+            int val1 = st.top();
+            st.pop();
+            
+            int val2 = st.top();
+            st.pop();
+            
+            switch (exp[i]) 
+            { 
+                case '+': st.push(val2 + val1); break; 
+                case '-': st.push(val2 - val1); break; 
+                case '*': st.push(val2 * val1); break; 
+                case '/': st.push(val2/val1); break; 
+            } 
+        } 
+    } 
+    
+    return st.top(); 
+}
