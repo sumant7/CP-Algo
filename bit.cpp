4f4ee8b750f05ee6438f1ee1ed2de1d3f33198a1
sumant7 <91867296+sumant7@users.noreply.github.com>
2022-05-19 16:56:38 +0530

    pair wise xor sum

--- bit.cpp
+++ bit.cpp
@@ -89,7 +89,33 @@ All the elements are integers and we know that size of int is 32 bits, so if
 we do the above for all the bits 0<=i<32 and sum all the results we will
 have our final answer.
 */
-
+long long int sumXOR(int arr[], int n)
+{
+    long long int sum = 0;
+    for (int i = 0; i < 32; i++)
+    {
+        //  Count of zeros and ones
+        int zc = 0, oc = 0;
+         
+        // Individual sum at each bit position
+        long long int idsum = 0;
+        for (int j = 0; j < n; j++)
+        {
+            if (arr[j] % 2 == 0)
+                zc++;
+            else
+                oc++;
+            arr[j] /= 2;
+        }
+         
+        // calculating individual bit sum
+        idsum = oc * zc * (1 << i);
+ 
+        // final sum   
+        sum += idsum;
+    }
+    return sum;
+}
 
 
 
