98764c2c24e785b0bd2bb057b709536c5f40c42c
sumant7 <91867296+sumant7@users.noreply.github.com>
2022-05-24 16:12:08 +0530

    bs

--- sort&search.cpp
+++ sort&search.cpp
@@ -15,25 +15,25 @@ using namespace std;
 //to search a number in an array with O(logN) complexity
 ll binarysearch(ll a[], ll n, ll k)    //k is the number to search
 {
-        ll l=0,r=n-1,mid;
-        while(r>=l)
+    ll l=0,r=n-1,mid;
+    while(r>=l)
+    {
+        mid= l+(r-l)/2;
+        if(a[mid]==k)
         {
-            mid= l+(r-l)/2;
-            if(a[mid]==k)
-            {
-                return mid;
-            }
-            if(a[mid]>k)
-            {
-                r=mid-1;
-            }
-            if(a[mid]<k)
-            {
-                l=mid+1;
-            }
+            return mid;
         }
-    return -1;
+        if(a[mid]>k)
+        {
+            r=mid-1;
+        }
+        if(a[mid]<k)
+        {
+            l=mid+1;
+        }
     }
+    return -1;
+}
 
 
 
