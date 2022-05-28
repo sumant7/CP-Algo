1aee03a7835250c553b25588bab98f5a598b29b6
sumant7 <91867296+sumant7@users.noreply.github.com>
2022-05-28 16:15:05 +0530

    roman to int

--- strings.cpp
+++ strings.cpp
@@ -61,3 +61,33 @@ string printRoman(int number)
     }
     return s;
 }
+
+
+//roman to int
+int romanToInt(string s) {
+    int sum = 0;
+    map<char,int> m;
+    m['I']=1;
+    m['V']=5;
+    m['X']=10;
+    m['L']=50;
+    m['C']=100;
+    m['D']=500;
+    m['M']=1000;
+    for (int i = 0; i < s.size(); i++) 
+    {
+        int check1 = 0, check2 = 0;
+        check1 = m[s[i]];
+        check2 = m[s[i+1]];
+        if (check1 >= check2) 
+        {
+            sum += check1;
+        }
+        else if (check1 < check2)  //like IV= 5-1=4
+        {
+            sum += (check2 - check1);
+            i++;
+        }
+    }
+    return sum;
+}
